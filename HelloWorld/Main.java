79eec6f5eb83ad99cd9b56a55c5d058c3c949433
tonya <tyrsenko.tonya@gmail.com>
2018-02-12 15:23:25 +0200

    testHello

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world
-        master
+        System.out.println("Hello world");
+        String unread = "TEST";
     }
 }
