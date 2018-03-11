22516be6df1e16c8b66ff7480edb60ec9ad079b9
tyrsenko <tyrsenko.tonya@gmail.com>
2018-03-11 16:46:31 +0200

    testbranch_ftesttodev

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -3,7 +3,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-       System.out.println("Hello world!!! More happiness");
+       System.out.println("Hello world!!! test");
        System unread = "TEST";
     }
 }
